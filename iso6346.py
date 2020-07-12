3a2aa6a45c706a8f9d55a745059797bbfe93ca80
Arsh Sharma <arshsharma25@gmail.com>
2020-07-12 15:23:51 +0530

    Followed PEP methodologies for spaces and added property for private attribute

--- iso6346.py
+++ iso6346.py
@@ -3,8 +3,6 @@ ISO 6346 shipping container codes.
 """
 
 
-
-
 def create(owner_code: object, serial: object, category: object = 'U') -> object:
     """Create an ISO 6346 shipping container code.
 
@@ -25,20 +23,15 @@ def create(owner_code: object, serial: object, category: object = 'U') -> object
     if not (len(owner_code) == 3 and owner_code.isalpha()):
         raise ValueError("Invalid ISO 6346 owner code '{}'".format(owner_code))
 
-
     if category not in ('U', 'J', 'Z', 'R'):
         raise ValueError("Invalid ISO 6346 category identifier '{}'".format(category))
-
 
     if not (len(serial) == 6 and serial.isdigit()):
         raise ValueError("Invalid ISO 6346 serial number")
 
-
     raw_code = owner_code + category + serial
     full_code = raw_code + str(check_digit(raw_code))
     return full_code
-
-
 
 
 def check_digit(raw_code):
@@ -52,10 +45,8 @@ def check_digit(raw_code):
     Returns:
         An integer check digit between 0 and 9 inclusive.
     """
-    s = sum(code(char) * 2**index for index, char in enumerate(raw_code))
+    s = sum(code(char) * 2 ** index for index, char in enumerate(raw_code))
     return s % 11 % 10
-
-
 
 
 def code(char):
@@ -70,8 +61,6 @@ def code(char):
         An integer code equivalent to the supplied character.
     """
     return int(char) if char.isdigit() else letter_code(char)
-
-
 
 
 def letter_code(letter):
--- shipping.py
+++ shipping.py
@@ -1,8 +1,8 @@
 import iso6346
 
+
 class ShippingContainer:
     next_serial: int = 1337
-
 
     @classmethod
     def _generate_serial(cls):
@@ -10,11 +10,9 @@ class ShippingContainer:
         cls.next_serial += 1
         return result
 
-
     @staticmethod
     def _make_bic_code(owner_code, serial):
         return iso6346.create(owner_code=owner_code, serial=str(serial).zfill(6))
-
 
     @classmethod
     # As the Child Class _init__ method has additional arguments so in order for these functions to support we use
@@ -22,11 +20,9 @@ class ShippingContainer:
     def create_empty(cls, owner_code, **kwargs):
         return cls(owner_code, contents=[], **kwargs)
 
-
     @classmethod
     def create_with_items(cls, owner_code, items, **kwargs):
         return cls(owner_code, contents=list(items), **kwargs)
-
 
     def __init__(self, owner_code, contents, **kwargs):
         self.owner_code = owner_code
@@ -34,23 +30,23 @@ class ShippingContainer:
         self.bic = self._make_bic_code(owner_code=owner_code, serial=ShippingContainer._generate_serial())
 
 
-
-
 class RefrigeratedShippingContainer(ShippingContainer):
-
-
     MAX_CELSIUS = 4.0
-
 
     # in order to make celsius keyword-only argument, we insert a singular * in the argument list before it
     def __init__(self, owner_code, contents, *, celsius, **kwargs):
+        """
+
+        :type celsius: object
+        """
         super.__init__(owner_code, contents, **kwargs)
         if celsius > RefrigeratedShippingContainer.MAX_CELSIUS:
             raise ValueError("Temperature too hot!")
-        self.celsius = celsius
+        self._celsius = celsius
 
-
-
+    @property
+    def celsius(self):
+        return self._celsius
 
     @staticmethod
     def _make_bic_code(owner_code: object, serial: object) -> object:
