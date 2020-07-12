632e87f360b67c491b11899e74d5953ddf8d96bf
Arsh Sharma <arshsharma25@gmail.com>
2020-07-12 17:39:37 +0530

    Added a Heated class and overwritten the celsius method for minimum temperature.

--- shipping.py
+++ shipping.py
@@ -2,6 +2,10 @@ import iso6346
 
 
 class ShippingContainer:
+
+    HEIGHT_FT = 8.5
+    WIDTH_FT = 8.0
+
     next_serial: int = 1337
 
     @classmethod
@@ -17,29 +21,36 @@ class ShippingContainer:
     @classmethod
     # As the Child Class _init__ method has additional arguments so in order for these functions to support we use
     # kwargs
-    def create_empty(cls, owner_code, **kwargs):
-        return cls(owner_code, contents=[], **kwargs)
+    def create_empty(cls, owner_code, length_ft, **kwargs):
+        return cls(owner_code, length_ft, contents=[], **kwargs)
 
     @classmethod
-    def create_with_items(cls, owner_code, items, **kwargs):
-        return cls(owner_code, contents=list(items), **kwargs)
+    def create_with_items(cls, owner_code, length_ft, items, **kwargs):
+        return cls(owner_code, length_ft, contents=list(items), **kwargs)
 
-    def __init__(self, owner_code, contents, **kwargs):
+    def __init__(self, owner_code, length_ft, contents, **kwargs):
         self.owner_code = owner_code
+        self.length_ft = length_ft
         self.contents = contents
         self.bic = self._make_bic_code(owner_code=owner_code, serial=ShippingContainer._generate_serial())
 
+    @property
+    def volume_ft3(self):
+        return ShippingContainer.HEIGHT_FT * ShippingContainer.WIDTH_FT * self.length_ft
+
 
 class RefrigeratedShippingContainer(ShippingContainer):
+
     MAX_CELSIUS = 4.0
+    FRIDGE_VOLUME_FT3 = 100
 
     # in order to make celsius keyword-only argument, we insert a singular * in the argument list before it
-    def __init__(self, owner_code, contents, *, celsius, **kwargs):
+    def __init__(self, owner_code, length_ft, contents, *, celsius, **kwargs):
         """
 
         :type celsius: object
         """
-        super.__init__(owner_code, contents, **kwargs)
+        super.__init__(owner_code, length_ft, contents, **kwargs)
         """Self encapsulation where even uses of attributes internal to the class go through the property getter and 
         setter rather than directly accessing underlying attribute. It is a technique for helping establish and 
         maintain class invariants such as temperature constraint """
@@ -76,6 +87,25 @@ class RefrigeratedShippingContainer(ShippingContainer):
     def fahrenheit(self, value):
         self.celsius = RefrigeratedShippingContainer._f_to_c(value)
 
+    @property
+    def volume_ft3(self):
+        return (
+            super().volume_ft3
+            - RefrigeratedShippingContainer.FRIDGE_VOLUME_FT3
+        )
+
     @staticmethod
     def _make_bic_code(owner_code: object, serial: object) -> object:
         return iso6346.create(owner_code=owner_code, serial=str(serial).zfill(6), category="R")
+
+
+class HeatedRefrigeratedShippingContainer(RefrigeratedShippingContainer):
+
+    MIN_CELSIUS = -20
+
+    @RefrigeratedShippingContainer.celsius.setter
+    def celsius(self, value):
+        if value <= HeatedRefrigeratedShippingContainer.MIN_CELSIUS:
+            raise ValueError("Temperature too cold!")
+        RefrigeratedShippingContainer.celsius.fset(self, value)
+
