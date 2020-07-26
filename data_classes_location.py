dc43ffda93ad2761a63d08d6eab0d273b6e2ecd8
Arsh Sharma <arshsharma25@gmail.com>
2020-07-26 18:58:45 +0530

    Added __post__init__ method used for validation in data classes

--- data_classes_location.py
+++ data_classes_location.py
@@ -8,12 +8,20 @@ from position import Position, EarthPosition
     repr=True, # enables __repr__
     eq=True,  # enables __eq__
     order=False, # enables __lt__, __gt__ etc i.e. less than or greater than
-    unsafe_hash=False,
-    frozen=False,
+    unsafe_hash=False, # configure __hash__ If not configured we cannot use hash based collections like set or dict in the underlying class
+    frozen=False, # it means immutable, if we make it True then we can use collections based on hash like set or dict in the underlying class
 )
 class Location:
     name: str
     position: Position
+
+    # if some value is empty we will not be able to checking
+    # this can be achieved by using __post__init__ method
+    # when this method is called all instance attributes will have been initialized therefore it doesn't take any argument beyond self
+    # it is good place to perform validation on data class instance construction
+    def __post__init__(self):
+        if self.name == "":
+            raise ValueError("Location name cannot be empty!")
 
 
 
